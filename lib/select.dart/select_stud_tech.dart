44760d1928e862f7cdb3cccb9dd6c589b761a1c4
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-17 23:47:38 -0700

    first commit

--- lib/select.dart/select_stud_tech.dart
+++ lib/select.dart/select_stud_tech.dart
@@ -1,57 +1,57 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
 
-class select extends StatefulWidget {
-  const select({ Key? key }) : super(key: key);
+// class select extends StatefulWidget {
+//   const select({ Key? key }) : super(key: key);
 
-  @override
-  State<select> createState() => _selectState();
-}
+//   @override
+//   State<select> createState() => _selectState();
+// }
 
-class _selectState extends State<select> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amber.shade300,
-      floatingActionButton: SpeedDial(
-        animatedIcon:AnimatedIcons.menu_close,
-        icon: Icons.share,
-        backgroundColor: Colors.black,
-        spacing: 12,
-        spaceBetweenChildren: 12,
-        closeManually: true,
-        onOpen: ()=>showToast("opened"),
-        onClose: () => showToast("closed"),
-        overlayOpacity: 0.4,
-        children:[
-          SpeedDialChild(
-            child: Icon(Icons.mail),
+// class _selectState extends State<select> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.amber.shade300,
+//       floatingActionButton: SpeedDial(
+//         animatedIcon:AnimatedIcons.menu_close,
+//         icon: Icons.share,
+//         backgroundColor: Colors.black,
+//         spacing: 12,
+//         spaceBetweenChildren: 12,
+//         closeManually: true,
+//         onOpen: ()=>showToast("opened"),
+//         onClose: () => showToast("closed"),
+//         overlayOpacity: 0.4,
+//         children:[
+//           SpeedDialChild(
+//             child: Icon(Icons.mail),
             
-           // backgroundColor: Colors.black,
-            label: "Mail",
-            onTap: ()=>showToast("mail"),
-          ),
-          SpeedDialChild(
-            child: Icon(Icons.copy),
-           // backgroundColor: Colors.black,
-            label: "Copy",
-          ),
-        ]
-      ),
-    );
+//            // backgroundColor: Colors.black,
+//             label: "Mail",
+//             onTap: ()=>showToast("mail"),
+//           ),
+//           SpeedDialChild(
+//             child: Icon(Icons.copy),
+//            // backgroundColor: Colors.black,
+//             label: "Copy",
+//           ),
+//         ]
+//       ),
+//     );
 
 
 
-  }
-}
+//   }
+// }
 
-Future showToast(String message)async{
-  await Fluttertoast.cancel();
+// Future showToast(String message)async{
+//   await Fluttertoast.cancel();
 
-  Fluttertoast.showToast(msg:message,fontSize: 18);
+//   Fluttertoast.showToast(msg:message,fontSize: 18);
 
-}
+// }
 
-/////////////////////////////////////////////
+// /////////////////////////////////////////////
