1845f4d87de2fcdb100f0652bd82885223e655c6
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-06-01 23:25:47 -0700

    first commit

--- lib/pages/signupscreen.dart
+++ lib/pages/signupscreen.dart
@@ -1,236 +1,236 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-// class SignUpScreen extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() => InitState();
-// }
+class SignUpScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => InitState();
+}
 
-// class InitState extends State<SignUpScreen> {
-//   @override
-//   Widget build(BuildContext context) => initWidget();
+class InitState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) => initWidget();
 
-//   Widget initWidget() {
-//     return Scaffold(
-//         body: SingleChildScrollView(
-//             child: Column(
-//               children: [
-//                 Container(
-//                   height: 200,
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90)),
-//                     color: new Color(0xFF405DE6),
-//                     gradient: LinearGradient(colors: [(new  Color(0xFF03d8dc)), new Color(0xFFa38af2)],
-//                       begin: Alignment.topCenter,
-//                       end: Alignment.bottomCenter,
-//                     ),
-//                   ),
-//                   child: Center(
-//                       child: Column(
-//                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                         crossAxisAlignment: CrossAxisAlignment.center,
-//                         children: [
-//                           Container(
-//                             margin: EdgeInsets.only(top: 50),
-//                             child: Image.asset(
-//                               "assets/grad.png",
-//                               height: 90,
-//                               width: 90,
-//                             ),
-//                           ),
-//                           Container(
-//                             margin: EdgeInsets.only(right: 20, top: 20),
-//                             alignment: Alignment.bottomRight,
-//                             child: Text(
-//                               "Register",
-//                               style: TextStyle(
-//                                   fontSize: 20,
-//                                   color: Colors.white
-//                               ),
-//                             ),
-//                           )
-//                         ],
-//                       )
-//                   ),
-//                 ),
+  Widget initWidget() {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90)),
+                    color: new Color(0xFF405DE6),
+                    gradient: LinearGradient(colors: [(new  Color(0xFF03d8dc)), new Color(0xFFa38af2)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 50),
+                            child: Image.asset(
+                              "assets/grad.png",
+                              height: 90,
+                              width: 90,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 20, top: 20),
+                            alignment: Alignment.bottomRight,
+                            child: Text(
+                              "Register",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                  ),
+                ),
 
-//                 Container(
-//                   alignment: Alignment.center,
-//                   margin: EdgeInsets.only(left: 20, right: 20, top: 70),
-//                   padding: EdgeInsets.only(left: 20, right: 20),
-//                   height: 54,
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(50),
-//                     color: Colors.grey[200],
-//                     boxShadow: [
-//                       BoxShadow(
-//                           offset: Offset(0, 10),
-//                           blurRadius: 50,
-//                           color: Color(0xffEEEEEE)
-//                       ),
-//                     ],
-//                   ),
-//                   child: TextField(
-//                     cursorColor: Color(0xFF03d8dc),
-//                     decoration: InputDecoration(
-//                       icon: Icon(
-//                         Icons.person,
-//                         color: Color(0xFF03d8dc),
-//                       ),
-//                       hintText: "Full Name",
-//                       enabledBorder: InputBorder.none,
-//                       focusedBorder: InputBorder.none,
-//                     ),
-//                   ),
-//                 ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xFF03d8dc),
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.person,
+                        color: Color(0xFF03d8dc),
+                      ),
+                      hintText: "Full Name",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
 
-//                 Container(
-//                   alignment: Alignment.center,
-//                   margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-//                   padding: EdgeInsets.only(left: 20, right: 20),
-//                   height: 54,
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(50),
-//                     color: Colors.grey[200],
-//                     boxShadow: [
-//                       BoxShadow(
-//                           offset: Offset(0, 10),
-//                           blurRadius: 50,
-//                           color: Color(0xffEEEEEE)
-//                       ),
-//                     ],
-//                   ),
-//                   child: TextField(
-//                     cursorColor: Color(0xFF03d8dc),
-//                     decoration: InputDecoration(
-//                       icon: Icon(
-//                         Icons.email,
-//                         color: Color(0xFF03d8dc),
-//                       ),
-//                       hintText: "Email",
-//                       enabledBorder: InputBorder.none,
-//                       focusedBorder: InputBorder.none,
-//                     ),
-//                   ),
-//                 ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xFF03d8dc),
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.email,
+                        color: Color(0xFF03d8dc),
+                      ),
+                      hintText: "Email",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
 
-//                 Container(
-//                   alignment: Alignment.center,
-//                   margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-//                   padding: EdgeInsets.only(left: 20, right: 20),
-//                   height: 54,
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(50),
-//                     color: Color(0xffEEEEEE),
-//                     boxShadow: [
-//                       BoxShadow(
-//                           offset: Offset(0, 20),
-//                           blurRadius: 100,
-//                           color: Color(0xffEEEEEE)
-//                       ),
-//                     ],
-//                   ),
-//                   child: TextField(
-//                     cursorColor: Color(0xFF03d8dc),
-//                     decoration: InputDecoration(
-//                       focusColor: Color(0xFF03d8dc),
-//                       icon: Icon(
-//                         Icons.phone,
-//                         color: Color(0xFF03d8dc),
-//                       ),
-//                       hintText: "Phone Number",
-//                       enabledBorder: InputBorder.none,
-//                       focusedBorder: InputBorder.none,
-//                     ),
-//                   ),
-//                 ),
-//                 Container(
-//                   alignment: Alignment.center,
-//                   margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-//                   padding: EdgeInsets.only(left: 20, right: 20),
-//                   height: 54,
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(50),
-//                     color: Color(0xffEEEEEE),
-//                     boxShadow: [
-//                       BoxShadow(
-//                           offset: Offset(0, 20),
-//                           blurRadius: 100,
-//                           color: Color(0xffEEEEEE)
-//                       ),
-//                     ],
-//                   ),
-//                   child: TextField(
-//                     cursorColor: Color(0xFF03d8dc),
-//                     decoration: InputDecoration(
-//                       focusColor: Color(0xFF03d8dc),
-//                       icon: Icon(
-//                         Icons.vpn_key,
-//                         color: Color(0xFF03d8dc),
-//                       ),
-//                       hintText: "Enter Password",
-//                       enabledBorder: InputBorder.none,
-//                       focusedBorder: InputBorder.none,
-//                     ),
-//                   ),
-//                 ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0xffEEEEEE),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 20),
+                          blurRadius: 100,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xFF03d8dc),
+                    decoration: InputDecoration(
+                      focusColor: Color(0xFF03d8dc),
+                      icon: Icon(
+                        Icons.phone,
+                        color: Color(0xFF03d8dc),
+                      ),
+                      hintText: "Phone Number",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0xffEEEEEE),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 20),
+                          blurRadius: 100,
+                          color: Color(0xffEEEEEE)
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Color(0xFF03d8dc),
+                    decoration: InputDecoration(
+                      focusColor: Color(0xFF03d8dc),
+                      icon: Icon(
+                        Icons.vpn_key,
+                        color: Color(0xFF03d8dc),
+                      ),
+                      hintText: "Enter Password",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
 
-//                 GestureDetector(
-//                   onTap: () {
-//                     // Write Click Listener Code Here.
-//                   },
-//                   child: Container(
-//                     alignment: Alignment.center,
-//                     margin: EdgeInsets.only(left: 20, right: 20, top: 70),
-//                     padding: EdgeInsets.only(left: 20, right: 20),
-//                     height: 54,
-//                     decoration: BoxDecoration(
-//                       gradient: LinearGradient(colors: [(new  Color(0xFF03d8dc)), new Color(0xFFa38af2)],
-//                           begin: Alignment.centerLeft,
-//                           end: Alignment.centerRight
-//                       ),
-//                       borderRadius: BorderRadius.circular(50),
-//                       color: Colors.grey[200],
-//                       boxShadow: [
-//                         BoxShadow(
-//                             offset: Offset(0, 10),
-//                             blurRadius: 50,
-//                             color: Color(0xffEEEEEE)
-//                         ),
-//                       ],
-//                     ),
-//                     child: Text(
-//                       "REGISTER",
-//                       style: TextStyle(
-//                           color: Colors.white
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//                 Container(
-//                   margin: EdgeInsets.only(top: 10, bottom: 10),
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       Text("Have Already Member?  "),
-//                       GestureDetector(
-//                         child: Text(
-//                           "Login Now",
-//                           style: TextStyle(
-//                               color: Color(0xFFa38af2)
-//                           ),
-//                         ),
-//                         onTap: () {
-//                           // Write Tap Code Here.
-//                           Navigator.pop(context);
-//                         },
-//                       )
-//                     ],
-//                   ),
-//                 )
-//               ],
-//             )
-//         )
-//     );
-//   }
-// }
+                GestureDetector(
+                  onTap: () {
+                    // Write Click Listener Code Here.
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    height: 54,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [(new  Color(0xFF03d8dc)), new Color(0xFFa38af2)],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.grey[200],
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 50,
+                            color: Color(0xffEEEEEE)
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      "REGISTER",
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Have Already Member?  "),
+                      GestureDetector(
+                        child: Text(
+                          "Login Now",
+                          style: TextStyle(
+                              color: Color(0xFFa38af2)
+                          ),
+                        ),
+                        onTap: () {
+                          // Write Tap Code Here.
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+        )
+    );
+  }
+}
