1bf1f7f89a96a59b37adfd61547bcc3e44b2b83b
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-06-05 23:47:12 -0700

    first commit

--- lib/controller/bottom_navigation_controller.dart
+++ lib/controller/bottom_navigation_controller.dart
@@ -1,11 +1,11 @@
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-import 'package:get/get.dart';
+// import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+// import 'package:get/get.dart';
 
-class BottonNavigationController extends GetxController{
-  var selectedIndex = 0.obs;
+// class BottonNavigationController extends GetxController{
+//   var selectedIndex = 0.obs;
 
 
-  void changeIndex(int index){
-  selectedIndex.value=index;
-  }
-}
+//   void changeIndex(int index){
+//   selectedIndex.value=index;
+//   }
+// }
